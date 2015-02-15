9057261f03d17a65522286c77c19ce8a4622db9c
whiteisnick <whiteisnick@gmail.com>
2015-02-15 14:57:39 -0500

    started on scripting the buttons

--- js/js1.js
+++ js/js1.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
-	
+	$('#back').on('click', function() {
+		
+	});
+	$('#full').on('click', function() {
+		
+	});
+	$('#fwd').on('click', function() {
+		
+	});
 	
 });
