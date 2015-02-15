43e95b4fbdaad9c53c0b170eeb2a704bdf296d50
whiteisnick <whiteisnick@gmail.com>
2015-02-15 16:42:43 -0500

    scripted the buttons to change the sample image and removed the full button. added images. changed the button content.

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
 
+	var currentSample = 1;
 	$('#back').on('click', function() {
-		
-	});
-	$('#full').on('click', function() {
-		
+		if (currentSample > 1) {
+			currentSample = currentSample - 1;
+			$('.samples-photo').toggle();
+			$('.samples-photo').html('<img class="img-responsive-height" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+			$('.samples-photo').fadeToggle(1000);
+			return(currentSample)
+		}
 	});
 	$('#fwd').on('click', function() {
-		
+		if (currentSample < 3) {
+			currentSample = currentSample + 1;
+			$('.samples-photo').toggle();
+			$('.samples-photo').html('<img class="img-responsive-height" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+			$('.samples-photo').fadeToggle(1000);
+			return(currentSample)
+		}
 	});
 	
 });
