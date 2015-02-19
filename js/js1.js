7012cdbdd2e61d7355af2a20fb3b7b0efeffb3d7
whiteisnick <whiteisnick@gmail.com>
2015-02-19 17:33:09 -0500

    removed old sample images and added new ones. edited javascript to properly handle new content.

--- js/js1.js
+++ js/js1.js
@@ -3,31 +3,63 @@ $(document).ready(function(){
 	var currentSample = 1;
 	$('#back').on('click', function() {
 		if (currentSample > 1) {
-			currentSample = currentSample - 1;
-			$('#sample').toggle();
+			if ((currentSample === 2) || (currentSample === 5)) {
+				currentSample = currentSample - 1;
+				$('.samples-photo').toggle();
+				$('.samples-photo').removeClass('col-xs-8 col-xs-offset-2').addClass('col-xs-10 col-xs-offset-1');
+				$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('.samples-photo').fadeToggle(700);
+				return(currentSample);
+			} else if(currentSample === 4) {
+				currentSample = currentSample - 1;
+				$('.samples-photo').toggle();
+				$('.samples-photo').removeClass('col-xs-10 col-xs-offset-1').addClass('col-xs-8 col-xs-offset-2');
+				$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('.samples-photo').fadeToggle(700);
+				return(currentSample);
+			} else {
+				currentSample = currentSample -1;
+				$('.samples-photo').toggle();
+				$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('.samples-photo').fadeToggle(700);
+			};
+		} else {
+			currentSample = 6;
+			$('.samples-photo').toggle();
+			$('.samples-photo').removeClass('col-xs-10 col-xs-offset-1').addClass('col-xs-8 col-xs-offset-2');
 			$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
-			$('#sample').fadeToggle(700);
-			return(currentSample);
-		}else {
-			currentSample = 3;
-			$('#sample').toggle();
-			$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
-			$('#sample').fadeToggle(700);
+			$('.samples-photo').fadeToggle(700);
 			return(currentSample);
 		};
 	});
 	$('#next').on('click', function() {
-		if (currentSample < 3) {
-			currentSample = currentSample + 1;
-			$('#sample').toggle();
-			$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
-			$('#sample').fadeToggle(700);
-			return(currentSample);
-		}else {
+		if (currentSample < 6) {
+			if ((currentSample === 1) || (currentSample === 4)) {
+				currentSample = currentSample + 1;
+				$('.samples-photo').toggle();
+				$('.samples-photo').removeClass('col-xs-10 col-xs-offset-1').addClass('col-xs-8 col-xs-offset-2');
+				$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('.samples-photo').fadeToggle(700);
+				return(currentSample);
+			} else if(currentSample == 3) {
+				currentSample = currentSample + 1;
+				$('.samples-photo').toggle();
+				$('.samples-photo').removeClass('col-xs-8 col-xs-offset-2').addClass('col-xs-10 col-xs-offset-1');
+				$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('.samples-photo').fadeToggle(700);
+				return(currentSample);
+			} else {
+				currentSample = currentSample + 1;
+				$('.samples-photo').toggle();
+				$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('.samples-photo').fadeToggle(700);
+			}
+		} else {
 			currentSample = 1;
-			$('#sample').toggle();
+			$('.samples-photo').toggle();
+			$('.samples-photo').removeClass('col-xs-8 col-xs-offset-2').addClass('col-xs-10 col-xs-offset-1');
 			$('#sample').html('<img class="img-responsive-height img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
-			$('#sample').fadeToggle(700);
+			$('.samples-photo').fadeToggle(700);
 			return(currentSample);
 		};
 	});
